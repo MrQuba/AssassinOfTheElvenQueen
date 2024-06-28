4b986c563574eb536c58be27806181120c74bc91
MrQuba <mrquba4@gmail.com>
2024-06-28 07:53:14 +0200

    create logger

--- main.cpp
+++ main.cpp
@@ -5,12 +5,16 @@
 #include "src/Characters/Entity.hpp"
 #include "src/Characters/Player.hpp"
 #include "src/Types/Types.hpp"
-
+#include "src/Logging/Logger.hpp"
 
 int main() {
+    Log("Creating Window object...");
     game::Window* window = new game::Window(sf::Vector2u(SIZE::WindowWidth, SIZE::WindowHeight));
+    Log("Creating Player object...");
     Player* player = new Player(PATH::playerSprite, Area(0, 0, 32, 32));
+    Log("Creating Event object...");
     sf::Event* event = new sf::Event();
+    Log("Entering Game Loop...");
     while (window->isOpen()) {
         ///////////////////////////////////////////////////
         //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -41,15 +45,20 @@ int main() {
         ///////////////////////////////////////////////////
         //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     }
+    Log("Quitting Game Loop...");
     ///////////////////////////////////////////////////
     //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     //Deleting objects at finished program
     ///////////////////////////////////////////////////
     //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    Log("Deleting Player object...");
     delete player;
+    Log("Deleting Event object...");
     delete event;
+    Log("Deleting Window object...");
     delete window;
     ///////////////////////////////////////////////////
     //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    Log("Qutting program...");
     return EXIT_SUCCESS;
 }
---
+++ src/Logging/Logger.hpp
@@ -0,0 +1,34 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <mutex>
+#pragma once
+class Logger {
+public:
+    // Delete copy constructor and assignment operator
+    Logger(const Logger&) = delete;
+    Logger& operator=(const Logger&) = delete;
+
+    static Logger& getInstance() {
+        static Logger instance;
+        return instance;
+    }
+
+    void log(const std::string& message) {
+        std::lock_guard<std::mutex> guard(mutex_);
+        std::cout << message << std::endl;
+        logfile_ << message << std::endl;
+    }
+
+private:
+    Logger() : logfile_("logs/log.txt", std::ios_base::out | std::ios_base::app) {}
+    ~Logger() { logfile_.close(); }
+
+    std::ofstream logfile_;
+    std::mutex mutex_;
+};
+#define Log(message) \
+    Logger::getInstance().log(message)
+#endif 
